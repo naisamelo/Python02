839831572b9c735ba17f71724897e271910d983d
Naísa Melo <naisamelo@gmail.com>
2022-09-22 21:02:59 -0300

    Create aula_12.py

---
+++ Condições em Python/aula_12.py
@@ -0,0 +1,10 @@
+nome = str(input('Qual é o seu nome? '))
+if nome == 'Naisa':
+    print('Que nome bonito!')
+elif nome == 'Pedro' or nome == 'João' or nome == 'Paulo' or nome == 'Maria':
+    print('Seu nome é bem popular no Brasil!')
+elif nome in 'Ana Claúdia Jéssica Juliana':
+    print('Belo nome feminino!')
+else:
+    print('Seu nome é bem normal')
+print('Tenha um bom dia, {}!'.format(nome))
