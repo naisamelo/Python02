6dcbb58b11c859ea830e91312182b5302ea54585
Naísa Melo <naisamelo@gmail.com>
2022-10-05 19:19:06 -0300

    Create teste.py

---
+++ Condições em Python/teste.py
@@ -0,0 +1,10 @@
+a = float(input('Digite o 1º nota: '))
+b = float(input('Digite o 2º nota: '))
+média = (a + b) / 2
+print('A média final é {}'.format(média))
+if média >= 7:
+    print('Aprovado')
+else:
+    print('Aluno está de recuperação')
+
+     
