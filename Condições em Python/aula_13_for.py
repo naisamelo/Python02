3812599e465feb69e01165d36e434ab3073ccdea
Naísa Melo <naisamelo@gmail.com>
2022-10-27 11:34:17 -0300

    Update aula_13_for.py

--- Condições em Python/aula_13_for.py
+++ Condições em Python/aula_13_for.py
@@ -1,3 +1,7 @@
-for c in range(0, 7, 2):
-    print(c)
-print('FIM')
+soma = 0
+for c in range(1, 11):
+    n = float(input('Digite o {}º valor: '.format(c)))
+    soma = soma + n
+media = soma/10
+print('A Média das notas é {:.2f}'.format(media))
+
