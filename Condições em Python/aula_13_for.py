05bb4f7dbd6144560f57c373e26798b9ea589733
Naísa Melo <naisamelo@gmail.com>
2022-10-27 20:11:10 -0300

    desafios 46, 47 e 48
    Aula 13 - For

--- Condições em Python/aula_13_for.py
+++ Condições em Python/aula_13_for.py
@@ -1,6 +1,6 @@
 soma = 0
 for c in range(1, 11):
-    n = float(input('Digite o {}º valor: '.format(c)))
+    n = float(input('Digite o {}º nota: '.format(c)))
     soma = soma + n
 media = soma/10
 print('A Média das notas é {:.2f}'.format(media))
