3de1d48db201e1d0d320b34be6b7a0f1c7845852
Naísa Melo <naisamelo@gmail.com>
2022-11-14 09:59:36 -0300

    Create ex_56_analise_completa.py

---
+++ Condições em Python/ex_56_analise_completa.py
@@ -0,0 +1,24 @@
+med_idade = 0
+tot_idade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+
+for p in range(1, 5):
+    print('-----{}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: '))
+    tot_idade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+med_idade = tot_idade / 4
+print('Média de idade {}'.format(med_idade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(totmulher20))
