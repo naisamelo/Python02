ae723ef3bbccb8ace161fba2de894ddd0ad891e0
naisamelo <naisamelo@gmail.com>
2023-11-08 20:15:36 -0300

    testando a biblioteca pyautogui

--- Condições em Python/teste.py
+++ Condições em Python/teste.py
@@ -1,2 +1,12 @@
-n = 25 - 5 + 4 ** 2
-print(n)
+import pyautogui
+import time
+
+pyautogui.PAUSE = 1
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+pyautogui.write("https://www.google.com/")
+pyautogui.press("enter")
+
+time.sleep(5)
