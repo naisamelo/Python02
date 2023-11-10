a78427b385500bad0bb92dbcd73911af0566abee
naisamelo <naisamelo@gmail.com>
2023-11-10 10:46:14 -0300

    mudança no link

--- Condições em Python/teste.py
+++ Condições em Python/teste.py
@@ -6,7 +6,7 @@ pyautogui.PAUSE = 1
 pyautogui.press("win")
 pyautogui.write("chrome")
 pyautogui.press("enter")
-pyautogui.write("https://www.google.com/")
+pyautogui.write("https://www.amazon.com.br/")
 pyautogui.press("enter")
 
 time.sleep(5)
