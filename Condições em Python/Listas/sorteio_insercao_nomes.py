3b7fc5320ddeba24c1e5c87974e96e99903dffea
Naísa Melo <naisamelo@gmail.com>
2023-12-05 15:41:41 -0300

    Exemplos de listas

---
+++ Condições em Python/Listas/sorteio_insercao_nomes.py
@@ -0,0 +1,3 @@
+import random
+nomes = input('1 nome: '), input('2 nome: '), input('3 nome: ')
+print('Os alunos sorteado é: {}'.format(random.choice(nomes)))
--- Condições em Python/teste.py
+++ Condições em Python/teste.py
@@ -1,7 +1,8 @@
-n = int(input("Digite um número para ver a tabuada: "))
+"""n = int(input("Digite um número para ver a tabuada: "))
 if 1 < n > 10:
     print("Número Inválido! Digite novamente")
     n = int(input("Digite novamente"))
 else:
     for c in range(1, 11):
-        print("{} x {} = {}".format(n, c, n*c))
+        print("{} x {} = {}".format(n, c, n*c))"""
+
