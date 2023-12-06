5b91dd4d64cdf2490c624054070c9f32243bae51
Naísa Melo <naisamelo@gmail.com>
2023-12-06 08:22:14 -0300

    adicionando while para controlar a leitura

--- Condições em Python/Listas/sorteio_insercao_nomes.py
+++ Condições em Python/Listas/sorteio_insercao_nomes.py
@@ -1,3 +1,11 @@
 import random
-nomes = input('1 nome: '), input('2 nome: '), input('3 nome: ')
+# nomes = input('1 nome: '), input('2 nome: '), input('3 nome: ')
+nomes = []
+c = 1
+while c <= 5:
+    nomes.append(input('Nome do aluno: '))
+    c = c + 1
+
 print('Os alunos sorteado é: {}'.format(random.choice(nomes)))
+
+
